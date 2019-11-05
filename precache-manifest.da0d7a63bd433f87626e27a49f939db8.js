1aeb1832e6b180e56e55cce7c488fa76fe566564
Fatemeh Pakpour <fatemehpakpour64@gmail.com>
2019-11-05 20:58:33 +0100

    Updates

---
+++ precache-manifest.da0d7a63bd433f87626e27a49f939db8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0fc99047cf15f4b8a625e8fbf5fe185",
+    "url": "/Giphy-Search/index.html"
+  },
+  {
+    "revision": "a2eacfec7160b7e537fa",
+    "url": "/Giphy-Search/static/css/main.046e43b5.chunk.css"
+  },
+  {
+    "revision": "64b26249a61e505568cf",
+    "url": "/Giphy-Search/static/js/2.6fa7896b.chunk.js"
+  },
+  {
+    "revision": "a2eacfec7160b7e537fa",
+    "url": "/Giphy-Search/static/js/main.07221c02.chunk.js"
+  },
+  {
+    "revision": "2df64f4e10dfc7e634ed",
+    "url": "/Giphy-Search/static/js/runtime-main.d765663c.js"
+  }
+]);
